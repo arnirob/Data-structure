09589b542733f6eade82ff918bbd2bb917f6674b
Md. Abdur Rouf Nirob <arnirob741@gmail.com>
2019-03-31 00:21:33 +0600

    Add files via upload

---
+++ reverse_linkedList_using_recursion.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	Node* next;
+};
+
+void Print(Node* hd)
+{
+	if(hd == NULL) return;
+	printf("%d ",hd->data);
+	Print(hd->next);
+}
+
+void Insert(Node* hd, int n)
+{
+	Node* temp = new Node();
+	temp->data = n;
+	temp-> next = NULL;
+	while(hd->next != NULL)
+	{
+		hd = hd->next;
+	}
+	hd->next = temp;
+}
+
+void Revers(Node* hd, Node* hd1)
+{
+	if(hd1->next == NULL)
+	{
+		hd->next = hd1;
+		return;
+	}
+	Revers(hd,hd1->next);
+	Node* temp = new Node();
+	temp = hd1->next;
+	temp->next = hd1;
+	hd1->next = NULL;
+}
+
+int main()
+{
+	Node* head = new Node();
+	Insert(head,1);
+	Insert(head,2);
+	Insert(head,3);
+	Insert(head,4);
+	Insert(head,5);
+	Print(head->next);
+	puts("");
+	Revers(head, head->next);
+	Print(head->next);
+	puts("");	
+	return 0;
+}
